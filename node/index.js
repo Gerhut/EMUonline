d98825ff4db410b79a69434eebabd845b928e769
Gerhut <Gerhut@GMail.com>
2014-03-21 11:54:37 +0800

    increase proformance of screenshot transfer

--- node/index.js
+++ node/index.js
@@ -9,31 +9,31 @@ screenshot.height = 160
 var server = new BinaryServer({port: 3000})
 
 server.on('connection', function (client) {
-  console.log('client connected.')
-  console.log(server._server.clients.length + ' online.')
+  console.log(Object.keys(server.clients).length + ' online.')
   client.on('close', function () {
-    console.log('client disconnected.')
-    console.log(server._server.clients.length + ' online.')
+    console.log(Object.keys(server.clients).length + ' online.')
   })
-
-  client.on('stream', function (stream, meta) {
-    stream.on('data', function (data) {
-      input.joypad(data.key, data.status)
-    })
-  })
+  
+  initScreenshotStream(client)
+  initJoypadStream(client)
 })
 
+function initScreenshotStream(client) {
+  client.streams.screenshot = client.createStream('screenshot')
+}
+
+function initJoypadStream(client) {
+  client.streams.joypad = client.createStream('joypad').on('data', function (data) {
+    if ('key' in data && 'status' in data)
+      input.joypad(data.key, data.status)
+  })
+}
 
 screenshot.on('screenshot', function (data) {
+  var stream
   for (var id in server.clients) {
-    try {
-      var client = server.clients[id]
-      if (Object.keys(client.streams).length === 0) {
-        client.send(data, 'screenshot')
-      }
-    } catch(e) {
-      console.log(e)
-    }
+    stream = server.clients[id].streams.screenshot
+    stream.write(data)
   }
 })
 screenshot.start()
