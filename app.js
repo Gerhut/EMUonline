1a2a149e376512f783f72ef5a1ece7a82fa08d06
George Chung <Gerhut@GMail.com>
2014-04-01 00:30:46 +0800

    Ap fool~

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var client, hellos = []
 
 function hello(name) {
-  document.getElementById('fool').setAttribute('id', '')
-  document.getElementsByTagName('h1')[0].innerText = '愚人节快乐～'
-  hellos.forEach( function (hello) { hello(name); } )
+  setTimeout(function () {
+    document.getElementById('fool').setAttribute('id', '')
+    document.getElementsByTagName('h1')[0].innerText = '愚人节快乐～'
+    hellos.forEach( function (hello) { hello(name); } )
+  }, 10000);
 }
