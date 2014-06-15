0da10bdd1d92ab592da6a52f3938347f9ca952fd
George Chung <Gerhut@GMail.com>
2014-06-15 17:34:22 +0800

    add input settings

--- node/input.js
+++ node/input.js
@@ -1,9 +1,11 @@
 var dgram = require('dgram')
 var socket = dgram.createSocket('udp4')
+var options = require('./input.json')
 
 function joypad(key, status, callback) {
+  if (!(key in options.key.allows)) return
   var message = new Buffer(['joypad', key, status].join(' '))
-  socket.send(message, 0, message.length, module.exports.port, '127.0.0.1', callback)
+  socket.send(message, 0, message.length, options.port, '127.0.0.1', callback)
 }
 
 exports = module.exports = {
@@ -11,7 +13,6 @@ exports = module.exports = {
 }
 
 if (require.main === module) {
-  module.exports.port = 682
   joypad(process.argv[2] || 'A', process.argv[3] || 1, function () {
     process.exit(0)
   })
